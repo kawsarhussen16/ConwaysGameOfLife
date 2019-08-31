f53d584106300a4f8e89aa4d9040080973e6156d
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-31 13:57:01 -0400

    Grid class implemented

--- conways-life/src/App.js
+++ conways-life/src/App.js
@@ -55,6 +55,37 @@ class Box extends React.Component {
   }
 }
 
+class Grid extends React.Component {
+  render() {
+    const width = (this.props.cols * 16) + 1;
+    var rowsArr = [];
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            col={j}
+            row={i}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+    return (
+      <div className=" grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    )
+  }
+}
+
+
 function arrayClone(arr) {
   return JSON.parse(JSON.stringify(arr));
 }
