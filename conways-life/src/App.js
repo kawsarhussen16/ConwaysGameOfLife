826476d337bbb74ccd282296ff576ac50e569d07
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-31 14:45:43 -0400

    last polish

--- conways-life/src/App.js
+++ conways-life/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { ButtonToolbar } from 'react-bootstrap';
 import './App.css';
 
 class App extends React.Component {
@@ -65,6 +65,13 @@ class App extends React.Component {
   pauseButton = () => {
     clearInterval(this.intervalId);
   }
+  clear = () => {
+    var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+    this.setState({
+      gridFull: grid,
+      generation: 0
+    });
+  }
   componentDidMount() {
     this.selectRandomBox();
     this.playButton();
@@ -74,6 +81,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1> The Game of Life</h1>
+        <Buttons
+          playButton={this.playButton}
+          clear={this.clear}
+          restart={this.restart}
+          pause={this.pauseButton}
+        />
         <Grid
           gridFull={this.state.gridFull}
           rows={this.rows}
@@ -82,6 +95,7 @@ class App extends React.Component {
         />
 
         <h2> Generation : {this.state.generation} </h2>
+        <Rules />
 
       </div>
     );
@@ -89,9 +103,6 @@ class App extends React.Component {
 }
 
 class Box extends React.Component {
-  selectBox = () => {
-    this.props.selectBox(this.props.row, this.props.col);
-  }
   render() {
     return (
       <div
@@ -144,6 +155,28 @@ class Buttons extends React.Component {
           <button className="btn btn-default" onClick={this.props.restart} > Restart </button>
 
         </ButtonToolbar>
+      </div>
+    )
+  }
+}
+class Rules extends React.Component {
+  render() {
+    return (
+      <div className="rules">
+        <h1>Rules for Conway's Game of Life</h1>
+
+        <p>
+          <>At the heart of this game are four rules that determine if a cell is live or dead. All depend on how many of that cell's neighbors are alive. </>
+          <ul>
+            <li> <strong>Births: </strong>Each dead cell adjacent to exactly three live neighbors will become live in the next generation.</li>
+
+            <li> <strong>Death by isolation: </strong>Each live cell with one or fewer live neighbors will die in the next generation.  </li>
+            <li> <strong>Death by overcrowding: </strong>Each live cell with four or more live neighbors will die in the next generation.</li>
+            <li> <strong>Survival: </strong>Each live cell with either two or three live neighbors will remain alive for the next generation.  </li>
+
+          </ul>
+          <>  Another important fact about the rules for the game of life is that all rules apply to all cells at the same time.  </>
+        </p>
       </div>
     )
   }
