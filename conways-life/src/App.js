73f1b4b7bf8f5bf493d5f99a54fc49b718da330d
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-31 14:08:39 -0400

    Button class implemented

--- conways-life/src/App.js
+++ conways-life/src/App.js
@@ -133,6 +133,22 @@ class Grid extends React.Component {
   }
 }
 
+class Buttons extends React.Component {
+  render() {
+    return (
+      <div className="center">
+        <ButtonToolbar>
+          <button className="btn btn-default" onClick={this.props.playButton}> Play </button>
+          <button className="btn btn-default" onClick={this.props.pause} > Pause </button>
+          <button className="btn btn-default" onClick={this.props.clear}> Clear </button>
+          <button className="btn btn-default" onClick={this.props.restart} > Restart </button>
+
+        </ButtonToolbar>
+      </div>
+    )
+  }
+}
+
 
 function arrayClone(arr) {
   return JSON.parse(JSON.stringify(arr));
