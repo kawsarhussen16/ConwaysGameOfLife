5d70105e7a7f8edf286c98d530fe0318bf2360a2
Kawsar Hussen <kawsarhussen@gmail.com>
2019-08-31 13:43:22 -0400

    Constructor created

--- conways-life/src/App.js
+++ conways-life/src/App.js
@@ -2,25 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> The Game of Life</h1>
+
+        <h2> Generation : {this.state.generation} </h2>
+      </div>
+    );
+  }
 }
+
 
 export default App;
